98beba68a492f62e21037fc5324484c119fdff8b
大蒙 <59262424@qq.com>
2026-09-04 09:16:51 +0800

    refactor(articles): 优化重构PC端文章模块样式并支持多主题

--- app/articles/_src/pc/js/comment.js
+++ app/articles/_src/pc/js/comment.js
@@ -1,84 +1,74 @@
 $(function(){
 
-	var loadCommentList = function(url, data, page, callback){
-		
-		var url = url + '&page='+page;
-        var loading_html = '<div class="loading">加载中...</div>';
-		var empty_html = '<div class="empty">还没有评论哦！</div>';
+    var loadCommentList = function(url, data, page, callback){
+
+        var url = url + '&page='+page;
+        var loading_html = '<div class="state-box"><i class="fa fa-spinner fa-pulse"></i> 评论加载中…</div>';
+        var empty_html = '<div class="state-box state-empty"><i class="fa fa-comments-o"></i><p>还没有评论，快来抢沙发吧～</p></div>';
         $('[data-role="list-section"]').html(loading_html);
-		$.ajax({
-			type: 'get',
-			url: url,
-			data: data,
-			dataType:"json",
-			success: function(ret){
-                //console.log(ret);
-			    if (ret && ret.data.data.length > 0) {
-			        var html = '';
+        $.ajax({
+            type: 'get',
+            url: url,
+            data: data,
+            dataType: "json",
+            success: function(ret){
+                if (ret && ret.data.data.length > 0) {
+                    var html = '';
 
-			        html += '<div class="list clearfix">';
-			        var item_html = '';
-			        var thisItem = ret.data;
-			        for (var i = 0, len = thisItem.data.length; i < len; i++) {
-			            var n = thisItem.data[i];
+                    html += '<div class="comment-list clearfix">';
+                    var item_html = '';
+                    var thisItem = ret.data;
+                    for (var i = 0, len = thisItem.data.length; i < len; i++) {
+                        var n = thisItem.data[i];
 
-			            item_html += '<div class="item clearfix" data-type="comment_item" data-id="'+ n.id +'">';
-                            item_html += '<div class="avatar">';
-                                item_html += '<img src="'+ n.user_info.avatar64 +'" />';
+                        item_html += '<div class="comment-item clearfix" data-type="comment_item" data-id="'+ n.id +'">';
+                            item_html += '<div class="comment-avatar">';
+                                item_html += '<img src="'+ n.user_info.avatar64 +'" alt="'+ n.user_info.nickname +'">';
                             item_html += '</div>';
-                            item_html += '<div class="content">';
-                                item_html += '<div class="nickname">'+ n.user_info.nickname +'</div>';
-                                item_html += '<div class="comment-detail">';
-									item_html += '<p>' + n.content + '</p>';
-                                    item_html +='<div class="comment-bottom">';
-                                    item_html += '<div class="time">' + n.create_time_friendly_str + '</div>';
-									if(n.support_yesno == 1){
-										item_html += '<div class="support active" data-id="'+ n.id +'">';
-										item_html += '<i class="fa fa-heart" aria-hidden="true"></i>';
-                                        if(n.support == 0){
-                                            item_html += '<p>赞</p>';
-                                        }else{
-                                            item_html += '<p>' + n.support + '</p>';
-                                        }
-										item_html += '</div>';
-									}else{
-										item_html += '<div class="support" data-id="'+ n.id +'">';
-										item_html += '<i class="fa fa-heart-o" aria-hidden="true"></i>';
-                                        if(n.support == 0){
-                                            item_html += '<p>赞</p>';
-                                        }else{
-                                            item_html += '<p>' + n.support + '</p>';
-                                        }
-										item_html += '</div>';
-									}
-                                    item_html += '</div>';
+                            item_html += '<div class="comment-main">';
+                                item_html += '<div class="comment-user">';
+                                    item_html += '<span class="nickname">'+ n.user_info.nickname +'</span>';
+                                    item_html += '<span class="floor">#' + (i + 1) + '</span>';
+                                item_html += '</div>';
+                                item_html += '<div class="comment-text">' + n.content + '</div>';
+                                item_html += '<div class="comment-foot">';
+                                    item_html += '<span class="comment-time"><i class="fa fa-clock-o"></i>' + n.create_time_friendly_str + '</span>';
+                                    if(n.support_yesno == 1){
+                                        item_html += '<span class="comment-like active" data-id="'+ n.id +'">';
+                                        item_html += '<i class="fa fa-heart"></i>';
+                                        item_html += '<em class="num">' + (n.support > 0 ? n.support : '赞') + '</em>';
+                                        item_html += '</span>';
+                                    }else{
+                                        item_html += '<span class="comment-like" data-id="'+ n.id +'">';
+                                        item_html += '<i class="fa fa-heart-o"></i>';
+                                        item_html += '<em class="num">' + (n.support > 0 ? n.support : '赞') + '</em>';
+                                        item_html += '</span>';
+                                    }
                                 item_html += '</div>';
                             item_html += '</div>';
-			            item_html += '</div>';
-			        }
-					// item_html += '<div class="time mui-ellipsis">'+ n.create_time_friendly_str +'</div>';
-			        html += item_html;
+                        item_html += '</div>';
+                    }
+                    html += item_html;
                     html += '</div>';
-                    
+
                     $('[data-role="list-section"]').html(html);
-					//console.log(thisItem);
-					$('[data-role="list-section"] .title .total').text('('+thisItem.total+')');
-			  	}else{
-			  		if(page == 1){
+                }else{
+                    if(page == 1){
                         //第一页无数据说明没有内容
-			  			$('[data-role="list-section"]').html(empty_html);
-			  		}
+                        $('[data-role="list-section"]').html(empty_html);
+                    }
                 }
 
                 //执行回调
-				callback(ret);
-			},
-			error: function(){
-				//alert('加载失败');
-			}
-		});
-	}
+                callback(ret);
+            },
+            error: function(){
+                $('[data-role="list-section"]').html('<div class="state-box state-error"><i class="fa fa-exclamation-circle"></i><p>评论加载失败，请稍后重试</p></div>');
+                callback && callback();
+            }
+        });
+    }
 
-	window.loadCommentList = loadCommentList;
+    window.loadCommentList = loadCommentList;
 
 });
